3158586c59446d48e48fd49786fb01642ab59d65
James Ruskin <james@chocolatey.io>
2025-09-19 10:24:10 +0100

    (patch-my-pc) Fixes Update Logic
    The releases page we were scanning only deals with PMP Publisher at this
    point, it seems, so the build was failing to find anything.
    
    This Home Updater release notes page seems to be the right page for
    the free updater, though it's worth noting that the download link has
    changed since the 5.x release, and the 5.x release also only supports x64
    Windows (see: https://patchmypc.com/faq/what-happens-now-that-patch-my-
    pc-home-updater-version-4-is-deprecated/ for more details).

--- automatic/patch-my-pc/tools/chocolateyInstall.ps1
+++ automatic/patch-my-pc/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = 'patch-my-pc'
   file           = "$toolsDir\PatchMyPC.exe"
-  url            = 'https://patchmypc.net/freeupdater/PatchMyPC.exe'
-  checksum       = 'd5a2ddba0ee5c577268d69bbc129046d48f36c8306c8a57d4f6b2e2ded193202'
-  checksumType   = 'sha256'
+  url64          = 'https://patchmypc.net/freeupdater/PatchMyPC.exe'
+  checksum64     = 'd5a2ddba0ee5c577268d69bbc129046d48f36c8306c8a57d4f6b2e2ded193202'
+  checksumType64 = 'sha256'
 }
 
 Get-ChocolateyWebFile @packageArgs
--- automatic/patch-my-pc/update.ps1
+++ automatic/patch-my-pc/update.ps1
@@ -1,7 +1,5 @@
 ﻿Import-Module Chocolatey-AU
 
-$versions     = 'https://patchmypc.net/release-notes'
-
 function global:au_AfterUpdate {
   Remove-Item -Force "$PSScriptRoot\tools\*.exe"
 }
@@ -10,23 +8,27 @@ function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1"   = @{
       "(?i)(^\s*packageName\s*=\s*)'.*'"  = "`${1}'$($Latest.PackageName)'"
-      "(?i)^(\s*url\s*=\s*)'.*'"          = "`${1}'$($Latest.URL32)'"
-      "(?i)^(\s*checksum\s*=\s*)'.*'"     = "`${1}'$($Latest.Checksum32)'"
-      "(?i)^(\s*checksumType\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType32)'"
+      "(?i)^(\s*url64\s*=\s*)'.*'"        = "`${1}'$($Latest.URL64)'"
+      "(?i)^(\s*checksum64\s*=\s*)'.*'"     = "`${1}'$($Latest.Checksum64)'"
+      "(?i)^(\s*checksumType64\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType64)'"
     }
   }
 }
-function global:au_GetLatest {
-  $url32 = 'https://patchmypc.net/freeupdater/PatchMyPC.exe'
 
-  $version_page = Invoke-WebRequest -Uri $versions -UseBasicParsing
-  $re = New-Object regex("\>What(?:'|&#039;)s new in ([\d\.]+)")
-  $version32 = $re.Match($version_page.Content).Groups[1]
+function global:au_GetLatest {
+  $releasenotes = 'https://patchmypc.com/release-notes/production-release/home-updater-releases/'
+  $url = 'https://homeupdater.patchmypc.com/public/PatchMyPC-HomeUpdater-Portable.exe'
+
+  $version_page = Invoke-WebRequest -Uri $releasenotes -UseBasicParsing
+  $re = New-Object regex('<h2 class="wp-block-heading" id="h-[\d-]+-\d{2}-\w+-\d{4}(-preview-only)?">(?<Version>[\d\.]+) \((?<Date>.+?)\)(?<Preview> \(Preview Only\))?<\/h2>')
+  $versions = $re.Matches($version_page.Content).Where{
+    -not $_.Groups['Preview'].success  # Where we haven't matched the 'Preview Only' group
+  }.Groups.Where{$_.Name -eq 'Version'}.Value | Sort-Object {[version]$_}
 
   @{
-    URL32 = $url32
-    Version = $version32
+    URL64 = $url
+    Version = $versions[-1]
   }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor 64
